70a6f4b004c4c555addd11d5de3f9d7bdc95e74b
siphokazi-codex <aplenis@webmail.co.za>
2017-05-15 15:15:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// create a router
+app.get('/greetings/:id', function(req, res){
+  res.send('Hello ' + req.params.id);
+});
+
+//start the server
+var server = app.listen(3000, function(){
+var host = server.address().address;
+var port = server.address().port;
+
+  console.log("App listening at http://%s:%s", host, port);
+});
